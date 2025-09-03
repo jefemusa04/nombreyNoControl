d744a7317fb03b0e221479965ed46788e5f66ef9
jefemusa04e <yisusnando05@gmail.com>
2025-09-03 09:38:39 -0600

    Se agrego nombre y numero de control centrados

--- app/src/main/java/com/example/nombreynocontrol/MainActivity.kt
+++ app/src/main/java/com/example/nombreynocontrol/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.nombreynocontrol.ui.theme.NombreyNoControlTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +25,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NombreyNoControlTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android", clase = "Desarrollo android",
+//                        modifier = Modifier.padding(innerPadding)
+
+//                    )
+                    Surface (
+                        modifier = Modifier.fillMaxSize().padding(innerPadding),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+                        GreetingText(
+                            message = "Jesus Muñoz",
+                            from = "22130835",
+                            modifier = Modifier.padding()
+                        )
+                    }
                 }
             }
         }
@@ -31,17 +48,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(name: String, clase:String, modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier,
+            fontSize = 50.sp
+        )
+        Text(
+            text = "Jose Luis",
+            fontSize = 30.sp
+        )
+
+        Text(
+            text = "Descripcion de Android",
+            fontSize = 30.sp
+        )
+
+        Text(
+            text = "$clase",
+            fontSize = 30.sp
+        )
+    }
+    Column {
+        Text(text = "Hola Mundo")
+        Text(text = "Expresion en Content")
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NombreyNoControlTheme {
-        Greeting("Android")
+        Greeting("Android", "Desarrollo android")
+    }
+}
+
+@Composable
+fun GreetingText(message: String, from:String, modifier: Modifier){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize() 
+    ) {
+        Text(
+            text = message,
+            fontSize = 110.sp,
+            lineHeight = 120.sp,
+            color = androidx.compose.ui.graphics.Color.Green
+        )
+        Text(
+            text = from,
+            fontSize = 30.sp
+        )
     }
 }
